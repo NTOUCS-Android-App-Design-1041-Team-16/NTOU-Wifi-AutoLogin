f837af59c42160c2f46b124612cab46fd9c67f35
林博仁(Buo-Ren Lin) <Buo.Ren.Lin@gmail.com>
2015-11-30 19:51:09 +0800

    建立 Android 軟體專案雛型， fix #2
    Signed-off-by: 林博仁(Buo-Ren Lin) <Buo.Ren.Lin@gmail.com>

---
+++ app/src/test/java/tw/edu/ntou/cs/android_app_design/semester1041/team16/ntou_wifi_autologin/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package tw.edu.ntou.cs.android_app_design.semester1041.team16.ntou_wifi_autologin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
