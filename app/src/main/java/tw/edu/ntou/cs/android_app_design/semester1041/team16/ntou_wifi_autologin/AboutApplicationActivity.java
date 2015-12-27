e991bf2a03dfc2ee4a0b41e0f572d82a8a471957
09957010 林博仁 <Buo.Ren.Lin@gmail.com>
2015-12-28 07:11:00 +0800

    Refactor - 將 MainActivity 更名為關於應用軟體 Activity
    Signed-off-by: 09957010 林博仁 <Buo.Ren.Lin@gmail.com>

--- app/src/main/java/tw/edu/ntou/cs/android_app_design/semester1041/team16/ntou_wifi_autologin/AboutApplicationActivity.java
+++ app/src/main/java/tw/edu/ntou/cs/android_app_design/semester1041/team16/ntou_wifi_autologin/AboutApplicationActivity.java
@@ -9,12 +9,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class AboutApplicationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about_application);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_about_application, menu);
         return true;
     }
 
