9ab4cfbdf88f166a7f06a13d10ee9e7b5f3d0ed0
09957010 林博仁 <Buo.Ren.Lin@gmail.com>
2016-01-04 07:04:10 +0800

    登入設定介面：實現保存學號密碼
    Signed-off-by: 09957010 林博仁 <Buo.Ren.Lin@gmail.com>

--- app/src/main/java/tw/edu/ntou/cs/android_app_design/semester1041/team16/ntou_wifi_autologin/LoginSettings.java
+++ app/src/main/java/tw/edu/ntou/cs/android_app_design/semester1041/team16/ntou_wifi_autologin/LoginSettings.java
@@ -1,11 +1,16 @@
 package tw.edu.ntou.cs.android_app_design.semester1041.team16.ntou_wifi_autologin;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.util.Log;
 
 public class LoginSettings extends AppCompatActivity {
-
+	private static final String TAG = "LoginSettings";
+	private static final String CONFIG_FILENAME = "NTOU_Wifi_Autologin.LoginSettings";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,6 +19,49 @@ public class LoginSettings extends AppCompatActivity {
 		setSupportActionBar(toolbar);
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		loadPreferences();
 	}
 
+	@Override
+	protected void onStart(){
+		super.onStart();
+		loadPreferences();
+	}
+
+	@Override
+	protected void onPause(){
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop(){
+		super.onStop();
+
+		savePreferences();
+	}
+
+	public void loadPreferences(){
+
+		Log.i(TAG, "載入設定……");
+		SharedPreferences config = getSharedPreferences(CONFIG_FILENAME, Context.MODE_PRIVATE);
+
+		((EditText) findViewById(R.id.editText_login_settings_student_id)).setText(config.getString("student_id", ""));
+		((EditText) findViewById(R.id.editText_login_settings_password)).setText(config.getString("password", ""));
+	}
+
+	public void savePreferences(){
+		Log.i(TAG, "保存設定……");
+
+		SharedPreferences config = getSharedPreferences(CONFIG_FILENAME, Context.MODE_PRIVATE);
+
+		SharedPreferences.Editor config_editor = config.edit();
+		EditText student_id = (EditText) findViewById(R.id.editText_login_settings_student_id);
+		//Log.i(TAG, "讀到 student_id 為：".concat(student_id.getText().toString()));
+		config_editor.putString("student_id", student_id.getText().toString());
+		EditText password = (EditText) findViewById(R.id.editText_login_settings_password);
+		//Log.i(TAG, "讀到 password 為：".concat(password.getText().toString()));
+		config_editor.putString("password", password.getText().toString());
+
+		config_editor.commit();
+	}
 }
